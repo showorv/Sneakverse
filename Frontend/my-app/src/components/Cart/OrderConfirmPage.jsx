64dd9a1a9d32978cd1cd399d4e605a83565e596d
Showrov <yshowrov7@gmail.com>
2025-02-27 14:45:02 +0600

    confirmOder

--- Frontend/my-app/src/components/Cart/OrderConfirmPage.jsx
+++ Frontend/my-app/src/components/Cart/OrderConfirmPage.jsx
@@ -30,6 +30,13 @@ const checkout={
     }
 }
 export const OrderConfirmPage = () => {
+
+    //delivery date in 10days
+    const calculateEstimatedDelivery = (created)=>{
+        const orderDate = new Date(created);
+        orderDate.setDate(orderDate.getDate()+10);
+        return orderDate.toLocaleDateString();
+    }
   return (
     <div className='max-w-4xl mx-auto p-8 bg-white my-8 '>
         <h1 className='text-2xl lg:text-4xl text-emerald-700 font-bold text-center mb-8'>Thank you for your order</h1>
@@ -43,6 +50,35 @@ export const OrderConfirmPage = () => {
                         <h2 className='text-sm lg:text-xl'>Order Id: {checkout._id}</h2>
                         <p className='text-gray-700 text-xs lg:text-md'>Order Date: {new Date (checkout.createdAt).toLocaleDateString()}</p>
                     </div>
+
+                    <div>
+                        <p className='text-sm md:text-lg text-gray-600 font-semibold'>Delivery Date: {calculateEstimatedDelivery(checkout.createdAt)}</p>
+                    </div>
+                </div>
+
+                <div className='mb-10'>
+                    {checkout.checkoutItems.map((curElem)=>{
+                        return(
+                            <div className='flex items-center mb-4'>
+                                <img src={curElem.image} alt={curElem.name} className='w-16 h-16 object-cover rounded-md mr-4'/>
+                                <div>
+                                    <h4 className='text-black font-bold '>{curElem.name}</h4>
+                                    <p className=' text-gray-800 text-sm  '>{curElem.color} | {curElem.size}</p>
+                                </div>
+
+                                <div className='ml-auto text-right'>
+                                    <p className=' text-gray-800 text-md font-semibold '>${curElem.price}</p>
+                                    <p className=' text-gray-800 text-sm '>Quantity: {curElem.quantity}</p>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+
+                <div>
+                    {/* payment history  */}
+
+                    <h2 className='text-black '>Payment:</h2>
                 </div>
 
             </div>
