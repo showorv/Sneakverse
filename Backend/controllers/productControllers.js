357870d480cdae1c3e684db28d748cf54328fa83
Showrov <yshowrov7@gmail.com>
2025-03-14 03:03:54 +0600

    update and delete products

--- Backend/controllers/productControllers.js
+++ Backend/controllers/productControllers.js
@@ -56,4 +56,46 @@ const productsCreate = async (req,res) =>{
 }
 
 
-export default productsCreate;
+// update product by id
+
+const updateProduct = async (req, res) => {
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(
+            req.params.id,  // Find product by ID
+            { $set: req.body }, // Update only provided fields
+            { new: true, runValidators: true } // Return updated doc & validate fields
+        );
+
+        if (!updatedProduct) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(500).json({ message: "Error updating product", error: error.message });
+    }
+};
+
+//delete product by id
+
+const deleteProduct = async (req,res) =>{
+
+    try {
+        
+        //find the product by id
+
+        const product = await Product.findById(req.params.id)
+        if(product){
+            await product.deleteOne();
+            res.status(200).json({messgs:"Products deleted"})
+        }else{
+            res.status(404).json({messgs:"products not found"})
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(404).json({mssgs:"Error in deleteproduct", error:error.message})
+    }
+}
+
+
+export default {productsCreate, updateProduct, deleteProduct};
--- Backend/routes/productRoute.js
+++ Backend/routes/productRoute.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { userMiddleware } from "../middleware/User-middleware.js";
 import { adminMiddleware } from "../middleware/admin-middleware.js";
-import productsCreate from "../controllers/productControllers.js";
+import productControllers from "../controllers/productControllers.js";
+const {productsCreate, updateProduct, deleteProduct} = productControllers;
 
 
 const router = Router()
@@ -9,5 +10,16 @@ const router = Router()
 //create products
 //access private/admin
 router.route("/").post(userMiddleware,adminMiddleware,productsCreate)
+
+//update products
+//put /api/products/:id
+router.route("/:id").put(userMiddleware,adminMiddleware,updateProduct)
+
+//delete product by id
+
+//delete /api/products/:id
+
+router.route("/:id").delete(userMiddleware,adminMiddleware,deleteProduct)
+
 
 export default router
