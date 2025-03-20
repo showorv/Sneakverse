51a8b8739599232392712f2332fde78d1090f57e
Showrov <yshowrov7@gmail.com>
2025-03-20 17:33:05 +0600

    checkout and order model

---
+++ Backend/model/checkoutModel.js
@@ -0,0 +1,94 @@
+import mongoose, { mongo } from "mongoose";
+
+const checkoutItemSchema = new mongoose.Schema({
+    productId :{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true
+    },
+
+    name: {
+        type: String,
+        required: true
+    },
+    image:{
+        type: String,
+        required: true
+    },
+
+    price:{
+        type:Number,
+        required: true
+    }
+},
+{_id: false}
+)
+
+const checkoutSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required: true
+    },
+
+    checkoutItems: [checkoutItemSchema],
+    deliveryAddress: {
+        address: {
+            type: String,
+            required: true
+        },
+        city:{ 
+            type: String,
+            required: true
+        }
+    },
+
+    paymentMethod: {
+        type: String,
+         required: true
+    },
+
+    totalPrice:{
+        type: Number,
+            required: true
+    },
+
+    isPaid:{
+        type: Boolean,
+        default: false
+    },
+    paidAt: {
+        type: Date
+    },
+
+    paymentStatus:{
+        type: String,
+        enum: ["pending", "completed", "failed"],
+        default: "pending"
+    },
+
+    paymentDetails:{
+        type: mongoose.Schema.Types.Mixed 
+        
+        //It’s useful when the structure of paymentDetails is not fixed (varies for different payment methods like bKash, PayPal, etc.). //if pay in bkash then response {
+        //     "paymentID": "TRX123456",
+        //     "payerReference": "017xxxxxxxx",
+        //     "transactionStatus": "Completed",
+        //     "amount": 500
+        // }
+    },
+
+    isFinalized: {
+        type: Boolean,
+        default: false
+    },
+    finalizedDate: {
+        type: Date
+    },
+
+}, {timestamps: true})
+
+
+const checkoutSchemas = mongoose.model("Checkout", checkoutSchema)
+
+export default checkoutSchemas;
---
+++ Backend/model/orderModel.js
@@ -0,0 +1,94 @@
+import mongoose from "mongoose";
+
+const orderitemSchema = new mongoose.Schema({
+
+    productId :{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true
+    },
+
+    name: {
+        type: String,
+        required: true
+    },
+    image:{
+        type: String,
+        required: true
+    },
+
+    price:{
+        type:Number,
+        required: true
+    },
+    size: String,
+    color: String,
+    quantity:{
+        type: Number,
+        required: true
+    }
+},
+{_id: false})
+
+
+const orderSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required: true
+    },
+
+    orderItems: [orderitemSchema],
+    deliveryAddress: {
+        address: {
+            type: String,
+            required: true
+        },
+        city:{ 
+            type: String,
+            required: true
+        }
+    },
+    paymentMethod: {
+        type: String,
+         required: true
+    },
+
+    totalPrice:{
+        type: Number,
+            required: true
+    },
+
+    
+
+    isPaid:{
+        type: Boolean,
+        default: false
+    },
+    paidAt: {
+        type: Date
+    },
+
+    isDelivered: {
+        type: Boolean,
+        default:false
+    },
+    deliveredAt: {
+        type: Date
+    },
+    paymentStatus:{
+        type: String,
+        enum: ["pending", "completed", "failed"],
+        default: "pending"
+    },
+
+    status:{
+        type: String,
+        enum: ["Processing", "Delivered", "Cancelled"],
+        default: "Processing"
+    }
+}, {timestamps:true})
+
+
+const order = mongoose.model("OrderItem", orderSchema)
+export default order;
